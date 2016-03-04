5f7c799d24ebe532ae0f9597585c54b694030fbf
ShenMinXu <452280994@qq.com>
2016-03-04 10:28:34 +0800

    重定向至高级搜索页面提交表单返回结果
    #改动说明：登录微博后再重定向到高级搜索页面提交表单，返回搜索结果
    #改动说明：目前只做了提交一个表单的代码，理论上可以通过一个循环不断提交表单返回结果

--- weibo.cn.py
+++ weibo.cn.py
@@ -1,3 +1,5 @@
+#改动说明：登录微博后再重定向到高级搜索页面提交表单，返回搜索结果
+#改动说明：目前只做了提交一个表单的代码，理论上可以通过一个循环不断提交表单返回结果
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
@@ -8,7 +10,6 @@ Info
 - author : "xchaoinfo"
 - email  : "xchaoinfo@qq.com"
 - date   : "2016.2.27"
-
 '''
 import requests
 import re
@@ -21,14 +22,14 @@ agent = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
 headers = {
     "User-Agent": agent,
     "Host": "login.weibo.cn",
-    "Origin": "https://login.weibo.cn",
+    "Origin": "http://weibo.cn",
     "Referer": "https://login.weibo.cn/login/"
 }
+
 
 session = requests.session()
 
 url_login = 'https://login.weibo.cn/login/'
-
 
 def get_params(url_login):
     html = session.get(url_login, headers=headers)
@@ -37,7 +38,6 @@ def get_params(url_login):
     res = re.findall(pattern, html.text, re.S)
     # print(res)
     return res
-
 
 def get_cha(capId):
     cha_url = "http://weibo.cn/interface/f/ttt/captcha/show.php?cpt="
@@ -56,14 +56,11 @@ def get_cha(capId):
 
     return cha_code
 
-
-
 res = get_params(url_login)
 if res == []:
     print("你的网络有问题，请检查网络后重试")
 else:
     post_url, password, vk, capId = res[0]
-
 
 if __name__ == "__main__":
     cha_code = get_cha(capId)
@@ -73,18 +70,27 @@ if __name__ == "__main__":
         "mobile": email,
         "code": cha_code,
         "remember": "on",
-        "backURL": "http%3A%2F%2Fweibo.cn",
+       #"backURL": "http%3A%2F%2Fweibo.cn",
+        "backURL": "http%3A%2F%2Fweibo.cn%2Fsearch%2F&amp",#这里返回页面是高级搜索页面
         "backTitle": "手机新浪网",
         "tryCount": "",
         "vk": vk,
         "capId": capId,
         "submit": "登录",
     }
+    data = {"advancedfilter": "1",#高级搜索中的表单提交内容，如果高级选项中勾选了认证，图片等选项表单提交字段会有所变化，具体见审核元素
+            "keyword": "季建业",
+            "nick": "",
+            "starttime": "",
+            "endtime": "20160303",
+            "sort": "time",
+            "smblog": "搜索"
+     }
+
     postdata[password] = password_input
     post_url = url_login + post_url
     page = session.post(post_url, data=postdata, headers=headers)
     index = session.get("http://weibo.cn")
     print(index.text)
-
 
 
